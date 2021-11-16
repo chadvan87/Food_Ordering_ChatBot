2b3b342d84dbf85d9efa662af89fbba028980fda
Chad Văn <ngvan.dev87@gmail.com>
2021-11-16 14:18:25 +0700

    Update urls.py

--- chatbot/urls.py
+++ chatbot/urls.py
@@ -17,13 +17,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
 from django.conf.urls import url
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bot/', include('bot.urls')),  # tell django about the new url
 
     url(r'^media/(?P<path>.*)$', serve,
-        {'document_root':       settings.MEDIA_ROOT}),
+        {'document_root': settings.MEDIA_ROOT}),
     url(r'^static/(?P<path>.*)$', serve,
         {'document_root': settings.STATIC_ROOT}),
 ]
