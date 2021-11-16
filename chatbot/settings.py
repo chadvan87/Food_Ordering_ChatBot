a40e847bc5f38feffd2c7a7ea7d527183b14b8cd
Chad Văn <ngvan.dev87@gmail.com>
2021-11-16 13:52:49 +0700

    Update settings.py

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#5706__idw_g)hxop**z+k)ih-tglmz39pg2n3#u@617o9!27f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'chat-bot-nott-a-code.herokuapp.com']
 
 
 # Application definition
